0d5c8216f3b5838b8fd2a09ba7bc4d4d3c0b0daa
HenryRhee <henry.rhee@smartfoodnet.com>
2021-07-02 16:59:07 +0900

    ch03 추가

---
+++ ch03/src/main/java/com/ex3/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.ex3;
+
+import com.ex3.DemoApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoApplication.class);
+    }
+
+}
